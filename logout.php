3eab997fe4aa1ff0ae91f807aad794a75f011b02
RyanChristian <capiliryan258@gmail.com>
2024-11-15 23:56:49 +0800

    successfully add logout

--- logout.php
+++ logout.php
@@ -1,1 +1,7 @@
+<?php
+session_start(); // Start the session
+session_destroy(); // Destroy the session to log the user out
+header('Location: index.php'); // Redirect to the login page after logout
+exit;
+?>
 
